4d47dfd8075457e6ff481bc7f227995dc8a1e70a
Kelvin Morais <kelvintitomorais@hotmail.com>
2023-03-06 22:19:14 -0300

    ok

---
+++ v4/src/routes/product-route.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+router.post('/', (req, res, next) => {
+    res.status(201).send(req.body)
+})
+
+router.put('/:id', (req, res, next) => { //:id é para passar parametro pela URL
+    const id = req.params.id
+    res.status(200).send({
+        id: id,
+        item: req.body
+    })
+})
+
+router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body)
+})
+
+module.exports = router
