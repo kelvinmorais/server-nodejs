211e8b5604191300a1458e7e5e3b304830d5af7c
Kelvin Morais <kelvintitomorais@hotmail.com>
2023-03-16 21:52:37 -0300

    ok

--- v7_bin-src_nodemon_bodyParser_Rotas _Controllers_Mongoose/src/controllers/product-controller.js
+++ v7_bin-src_nodemon_bodyParser_Rotas _Controllers_Mongoose/src/controllers/product-controller.js
@@ -1,6 +1,63 @@
 const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send({
+                data: e
+            })
+        })
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send({
+                data: e
+            })
+        })
+}
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send({
+                data: e
+            })
+        })
+}
+
+exports.getByTag = (req, res, next) => {
+    Product
+        .find({
+            tags: req.params.tag,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data)
+        }).catch(e => {
+            res.status(400).send({
+                data: e
+            })
+        })
+}
+
+
 
 exports.post = (req, res, next) => {
     var product = new Product(req.body) 
